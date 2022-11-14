0597a6fd0ccf187211951ff7e702a82cf05821bd
jomar <jgil@village88.com>
2022-11-15 06:28:36 +0800

    thewall-restart

--- routers/app.router.js
+++ routers/app.router.js
@@ -1,11 +1,9 @@
 let ViewsRouter = require('./views.router');
 let UsersRouter = require('./users.router');
-let WallRouters = require('./wall.router');
 
 let AppRoutes = (app) => {
     app.use('/', ViewsRouter);
     app.use('/api/users', UsersRouter);
-    app.use('/api/wall', WallRouters);
 }
 
 module.exports = AppRoutes;
--- routers/views.router.js
+++ routers/views.router.js
@@ -5,32 +5,15 @@ const ViewsController = require('../controllers/views.controllers');
 let viewsController = new ViewsController();
 
 ViewsRouter.get('/login', (req, res) => {
-    if(req.session.general){
-        res.redirect('/wall')
-    }
-    else{
-        res.render('login');
-    }
+    res.render('login');
 });
 
 ViewsRouter.get('/register', (req, res) => {
-    if(req.session.general){
-        res.redirect('/wall')
-    }
-    else{
-        res.render('registration');
-    }
+    res.render('registration');
 });
 
 ViewsRouter.get('/', (req, res) => {
-    if(req.session.general){
-        res.redirect('/wall')
-    }
-    else{
-        res.render('login');
-    }
+    res.render('login');
 });
-
-ViewsRouter.get('/wall', viewsController.renderWall);
 
 module.exports = ViewsRouter;
